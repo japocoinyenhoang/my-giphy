0034b7b1fdc5a65e5610f5846704e7e16d41c24a
yenhoang <japocoin@gmail.com>
2021-05-09 01:22:51 +0200

    render gifs and change them with a button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+  const GIFS = ['https://media.giphy.com/media/EatwJZRUIv41G/giphy.gif', 
+  'https://media.giphy.com/media/jKpVHextCiB8c/giphy.gif'];
+
+  const OTHERGIFTS =['https://media.giphy.com/media/PDFQfRc80TTi0/giphy.gif', 
+  'https://media.giphy.com/media/EPcvhM28ER9XW/giphy.gif'] ;
+
+  const [gifs, setGifts] = useState(GIFS);
+
+  //enderiza cada veaz que se renderiza la pagina
+  //lo que antiguamemte era el COMPONENTDIDMOUNT 
+  //useEffect()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-content">
+        { gifs.map(singleGif => <img src={singleGif} />)}
+        <button onClick= {()=>setGifts(OTHERGIFTS)}>CAMBIAR GIFS</button>
+      </section>
     </div>
   );
 }
