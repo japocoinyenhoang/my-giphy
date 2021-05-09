c8c30c2bede30305cc98d9639de6750cc3c634ca
yenhoang <japocoin@gmail.com>
2021-05-09 02:21:23 +0200

    url refactored

--- src/App.js
+++ src/App.js
@@ -5,18 +5,19 @@ import getGifs from './services/getGifs';
 
 function App() {
 
-const [gifs, setGifts] = useState([]);
+const [gifs, setGifs] = useState([]);
   
 //se ejecuta cada vez que se renderiza el componente. Tenemos que ponerle un segundo parametro para que no entre en bucle ya que se actualiza el estado y se renderiza el componente, y se vuelve a actualizar el estado y a renderizar el componente....
 //useEffect(function () { setGifts(OTHERGIFTS)}, [])  //con el  [], solo se ejecuta una vez y es lo que antiguamemte era el COMPONENTDIDMOUNT 
+
 useEffect(function () {
-  getGifs.then (gifs => getGifs(gifs))
-  }, [])
+  getGifs({keyword:'rick'}).then(gifs => setGifs(gifs))
+  }, []);
+
   return (
     <div className="App">
       <section className="App-content">
         { gifs.map(singleGif => <img src={singleGif} />)}
-        
       </section>
     </div>
   );
--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -1,12 +1,16 @@
-const API_URL='https://api.giphy.com/v1/gifs/search?api_key=dIJrma20pSU6ymMwWnDbiaT7NFHeAGVa&q=panda&limit=10&offset=0&'
+const apiKey = 'dIJrma20pSU6ymMwWnDbiaT7NFHeAGVa'
+//const API_URL='https://api.giphy.com/v1/gifs/search?api_key=dIJrma20pSU6ymMwWnDbiaT7NFHeAGVa&q=panda&limit=10&offset=0&';
+export default function getGifs ({keyword ='panda'} = {})  {
+  
+    const API_URL=`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=10&offset=0&`;
 
-export default function getGifs ()  {
-    fetch(API_URL)
-    .then (res => res.json())
-    .then(response =>
-      {
-        const {data = []} = response
-        const gifs = data.map(image => image.images.downsized_medium.url)
-        return gifs
+    return fetch(API_URL)
+      .then (res => res.json())
+      .then(response =>{
+          const {data = []} = response
+          if (Array.isArray(data)){
+            const gifs = data.map(image => image.images.downsized_medium.url)
+            return gifs
+          }
       })
 }
