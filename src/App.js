67c8636416d9590cf932fff3a08f30663e2d4b41
yenhoang <japocoin@gmail.com>
2021-05-09 02:59:39 +0200

    app.js cleaned and services and states exported to listOfGifs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import './App.css';
-import getGifs from './services/getGifs';
 import ListOfGifs from './components/ListOfGifs';
 
 function App() {
 
-const [gifs, setGifs] = useState([]);
-  
-//se ejecuta cada vez que se renderiza el componente. Tenemos que ponerle un segundo parametro para que no entre en bucle ya que se actualiza el estado y se renderiza el componente, y se vuelve a actualizar el estado y a renderizar el componente....
-//useEffect(function () { setGifts(OTHERGIFTS)}, [])  //con el  [], solo se ejecuta una vez y es lo que antiguamemte era el COMPONENTDIDMOUNT 
-
-useEffect(function () {
-  getGifs({keyword:'rick'}).then(gifs => setGifs(gifs))
-  }, []);
-
   return (
     <div className="App">
       <section className="App-content">
-       <ListOfGifs gifs={gifs} />
+       <ListOfGifs keyword='panda' />
       </section>
     </div>
   );
--- src/components/ListOfGifs.js
+++ src/components/ListOfGifs.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import getGifs from '../services/getGifs';
 import Gif from './Gif'
 
-export default function ListOfGifs({gifs}) {
+export default function ListOfGifs({keyword}) {
+
+const [gifs, setGifs] = useState([]);
+  
+useEffect(function () {
+  getGifs({keyword}).then(gifs => setGifs(gifs))
+  }, [keyword]);
+
   return (
     gifs.map(({id, title, url}) => 
       <Gif  
